8f81deb6ae415ba46bac6fcf4e933f7718bdd3b3
QuanBaoCS <nguyenquanbao2632005@gmail.com>
2024-09-02 21:11:08 +0700

    update

--- Bank.java
+++ Bank.java
@@ -4,21 +4,24 @@ import java.util.List;
 
 public class Bank{
     //Atributte
-    private String name;
-    private List<Customer> customers;
-    private List<Account> accounts;
+    private String name; //Tên ngân hàng
+    private List<Customer> customers; //Danh sách khách hàng
+    private List<Account> accounts; //Danh sách tài khoản ngân hàng
     //Constructor
     public Bank(String name){
         this.name = name;
         this.customers = new ArrayList<>();
         this.accounts = new ArrayList<>();
     }
+    //thêm khách hàng mới 
     public void addCustomer(Customer customer){
         customers.add(customer);
     }
+    //thêm tài khoản mới 
     public void addAccount(Account account){
         accounts.add(account);
     }
+    // Truy xuất khách hàng bằng ID
     public Customer getCustomer (String customerID){
         for(Customer customer : customers){
             if (customer.getId().equals(customerID)){
@@ -27,4 +30,14 @@ public class Bank{
         }
         return null;
     }
+    // Truy xuất tài khoản bằng số tài khoản
+    public Account getAccount(String accountNumber) {
+        for (Account account : accounts) {
+            if (account.getAccountNumber().equals(accountNumber)) {
+                return account;
+            }
+        }
+        return null;
+    }
 }
+    
--- Customer.java
+++ Customer.java
@@ -1,3 +1,55 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Customer {
-    
+    private String customerID; //Mã định danh duy nhất cho 1 khách hàng
+    private  String name; //Tên khách hàng
+    private String address; //Địa chỉ khách hàng
+    private List<Account> accounts; // Danh sách tài khoản liên kết với khách hàng
+
+    public Customer(String customerID, String name, String address){
+        this.customerID = customerID;
+        this.name =  name;
+        this.address = address;
+        this.accounts = new ArrayList<>();
+    }
+    //Thêm tài khoản mới 
+    public void addAccount (Account account){
+        accounts.add(account);
+    }
+    //Tính tổng số dư trong tài khoản ngân hàng
+    public void getTotalBalance(){
+        double totalBalance = 0.0;
+        for (Account account : accounts) {
+            totalBalance += account.getBalance();
+        }
+        return totalBalance;
+    }
+    //Setter và Getter dữ liệu
+    public String getCustomerID(){
+        return customerID;
+    }
+    public void setCustomerID(String customerID){
+        this.customerID = customerID;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getAddress(){
+        return address;
+    }
+    public void setAddress(String address){
+        this.address = address;
+    }
+    public List<Account> getAccounts (){
+        return accounts;
+    }
+    public void setAccounts(List<Account>accounts){
+        this.accounts = accounts;
+    }
 }
+
+
